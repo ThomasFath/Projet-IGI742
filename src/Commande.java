8098ef738ce3d767ba87fc81737d37439950de84
Thomas <thomas.fath@wanadoo.fr>
2015-12-17 17:36:55 +0100

    17122015
    connecxion inscription et depose ok

--- src/Commande.java
+++ src/Commande.java
@@ -1,12 +1,11 @@
-
+// Classe gerant les commandes des ptofils pour les annonces
 public class Commande {
 	private String ref_commande;
 	private String date_commande;
 	private String etat_comande;
 	private String ref_annonce;
 
-	void creer(){
-		
+	void creer(){	
 	}
 	void changer_etat(){
 		System.out.println("Tu viens de changer l'état de la commande, bravo !");
