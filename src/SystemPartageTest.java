d10ada8f3d749d587e16a4868670eb9e5ee364a1
Ichinin <r.suntharasarma@gmail.com>
2016-01-06 11:51:11 +0100

    Limite du test unitaire

---
+++ src/SystemPartageTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SystemPartageTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		/*String arguments[]={"annonce1","description","profilecreateur","profilecreateur","profilecreateur","profilecreateur","profilecreateur","profilecreateur"};
+		String args[]= {"profilecreateur","profilecreateur","profilecreateur"};
+		String b [] = {};
+		Profil p = (Profil)Factory.create("Profil", arguments);
+		SystemPartage s = (SystemPartage)Factory.create("SystemPartage", b);
+		s.connexion(p.getLogin(), p.getMot_de_passe());
+		int nb_annonce_init = s.getListAnnonce().size();
+		p.poster_annonce(args);
+		assertEquals(s.getListAnnonce().size(),(nb_annonce_init+1));*/
+	}
+
+}
